1016af017e14a412add648e716d107d3e7372956
Olivia Bettaglio <olivia.bettaglio@gmail.com>
2017-01-20 21:53:29 -0800

    Initial commit.

---
+++ server.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+
+
+app = Flask(__name__)
+
+
+# Required to use Flask sessions and the debug toolbar
+app.secret_key = "ABC"
+
+
+@app.route('/')
+def show_homepage():
+    """Display homepage."""
+
+    return render_template("index.html")
+
+
+@app.route('/application-form')
+def show_app_form():
+    """Display application form."""
+
+    return render_template("application-form-flask.html")
+
+
+if __name__ == "__main__":
+    # We have to set debug=True here, since it has to be True at the
+    # point that we invoke the DebugToolbarExtension
+    app.debug = True
+
+    # Use the DebugToolbar
+    DebugToolbarExtension(app)
+
+    app.run(host="0.0.0.0")
